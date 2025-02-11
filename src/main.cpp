e3aef89871ad41042971e763f47af5a215a46936
Caio Guimaraes <caio.b.vilar@gmail.com>
2025-02-11 11:14:05 -0300

    Fixed build with dependencies working.

--- src/main.cpp
+++ src/main.cpp
@@ -1,2 +1,12 @@
-#include ""
-#include "IDBClient.hpp"
+#include "iostream"
+
+int main()
+{
+    std::cout << " Starting the BK Server..." << std::endl;
+    std::cout << " Loading the configuration..." << std::endl;
+    std::cout << " Connecting to the database..." << std::endl;
+    std::cout << " Server is running..." << std::endl;
+    std::cout << " Accepting new connections..." << std::endl;
+    std::cout << " Closing the BK Server..." << std::endl;
+    return 0;
+}
